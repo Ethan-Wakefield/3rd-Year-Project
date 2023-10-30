124c3a9a4951f2f3d4c6f22f375423d2b93e211b
Ethan Wakefield <ethanwakefield000@gmail.com>
2023-10-30 16:54:35 +0000

    RDF Triples Converted to Adjacency List

--- KGTest/KGTestSpaCy.py
+++ KGTest/KGTestSpaCy.py
@@ -1,18 +1,3 @@
-'''
-import urllib.request
-from bs4 import BeautifulSoup
-import spacy
-import neuralcoref
-
-nlp = spacy.load('en_core_web_sm')
-neuralcoref.add_to_pipe(nlp)
-doc1 = nlp('The town has a hill to the left of it. There is a valley to its right. Lucy lives in the valley, but she does not like it.')
-print(doc1._.coref_resolved)
-
-#install spacy 2.3.5
-#install python 3.7
-'''
-
 import spacy
 import crosslingual_coreference
 
@@ -22,6 +7,8 @@ import hashlib
 from spacy import Language
 from typing import List
 
+from collections import defaultdict
+
 from spacy.tokens import Doc, Span
 
 from transformers import pipeline
@@ -29,7 +16,7 @@ from transformers import logging
 logging.set_verbosity_error()
 
 
-
+#Take output from REBEL model and parse it, to create a list of dictionaries with fields "head", "type" (relation) and "tail". 
 def extract_triplets(text):
     triplets = []
     relation, subject, relation, object_ = '', '', '', ''
@@ -61,6 +48,8 @@ def extract_triplets(text):
         triplets.append({'head': subject.strip(), 'type': relation.strip(),'tail': object_.strip()})
     return triplets
 
+
+#REBEL model, to be used as a SpaCy pipeline component
 @Language.factory(
     "rebel",
     requires=["doc.sents"],
@@ -81,7 +70,6 @@ class RebelComponent:
         assert model_name is not None, ""
         self.triplet_extractor = pipeline("text2text-generation", model=model_name, tokenizer=model_name, device=device)
         self.entity_mapping = {}
-        self.counter = 0
         # Register custom extension on the Doc
         if not Doc.has_extension("rel"):
           Doc.set_extension("rel", default={})
@@ -94,43 +82,55 @@ class RebelComponent:
 
     def set_annotations(self, doc: Doc, triplets: List[dict]):
         for triplet in triplets:
-            doc._.rel[self.counter] = {"head": triplet['head'], "relation": triplet["type"], "tail": triplet['tail']}
-            self.counter = self.counter + 1
+            if triplet['head'] != triplet['tail']:
+                definer = triplet['head'] + triplet['type'] + triplet['tail']
+                doc._.rel[hash(definer)] = {"head": triplet['head'], "relation": triplet["type"], "tail": triplet['tail']}
 
     def __call__(self, doc: Doc) -> Doc:
         for sent in doc.sents:
             sentence_triplets = self._generate_triplets(sent)
-           
             self.set_annotations(doc, sentence_triplets)
         return doc
     
+
+#Form graph out of RDF triples developed by the model
+class DiGraph():
+    def __init__(self):
+        self.adjList = defaultdict(list)
+
+    def addRelation(self, rel_dict):
+        source = rel_dict['head']
+        
+        relation = rel_dict['relation']
+       
+        sink = rel_dict['tail']
+        self.adjList[source].append((relation, sink))
+        
+        
+
 
 DEVICE = -1 # Number of the GPU, -1 if want to use CPU
 
 # Add coreference resolution model
 coref = spacy.load("en_core_web_sm", disable=['ner', 'tagger', 'parser', 'attribute_ruler', 'lemmatizer'])
 coref.add_pipe("xx_coref", config={"chunk_size": 2500, "chunk_overlap": 2, "device": DEVICE})
-
 # Define rel extraction model
-
 rel_ext = spacy.load("en_core_web_sm", disable=['ner', 'lemmatizer', 'attribute_rules', 'tagger'])
 rel_ext.add_pipe("rebel", config={'device':DEVICE, 'model_name':'Babelscape/rebel-large'})
 
-input_text = "Napoleon Bonaparte (born Napoleone di Buonaparte; 15 August 1769 - 5 " \
-"May 1821), and later known by his regnal name Napoleon I, was a French military " \
-"and political leader who rose to prominence during the French Revolution and led " \
-"several successful campaigns during the Revolutionary Wars. He was the de facto " \
-"leader of the French Republic as First Consul from 1799 to 1804. As Napoleon I, " \
-"he was Emperor of the French from 1804 until 1814 and again in 1815. Napoleon's " \
-"political and cultural legacy has endured, and he has been one of the most " \
-"celebrated and controversial leaders in world history."
+#Test
+input_text = "The plague disease, caused by Yersinia pestis, is enzootic (commonly present) in populations of fleas carried by ground rodents, including marmots, in various areas including Central Asia, Kurdistan, Western Asia, Northern India and Uganda. Nestorian graves dating to 1338-39 near Lake Issyk Kul in Kyrgyzstan have inscriptions referring to plague and are thought by many epidemiologists to mark the outbreak of the epidemic, from which it could easily have spread to China and India. In October 2010, medical geneticists suggested that all three of the great waves of the plague originated in China. In China, the 13th century Mongol conquest caused a decline in farming and trading. However, economic recovery had been observed at the beginning of the 14th century. In the 1330s a large number of natural disasters and plagues led to widespread famine, starting in 1331, with a deadly plague arriving soon after. Epidemics that may have included plague killed an estimated 25 million Chinese and other Asians during the 15 years before it reached Constantinople in 1347."
 
 coref_text = coref(input_text)._.resolved_text
 print(coref_text)
-
 doc = rel_ext(coref_text)
-print("PLEASE")
+
+graph = DiGraph()
 for value, rel_dict in doc._.rel.items():
     print(f"{value}: {rel_dict}")
+    graph.addRelation(rel_dict)
 
-print("ended")
+print("\n")
+print("ADJLIST:")
+print(graph.adjList.items())
+
